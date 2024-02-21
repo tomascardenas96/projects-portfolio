9761434aa212fc3a9e258df7c7adddcbad8d8d32
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-02-20 22:35:59 -0300

    Auth guard done (protected route implement in home endpoint)

--- notes/backend/src/auth/auth.controller.ts
+++ notes/backend/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './guard/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +16,11 @@ export class AuthController {
   @Post('/login')
   login(@Body() user: LoginDto) {
     return this.authService.login(user);
+  }
+
+  @Get('/home')
+  @UseGuards(AuthGuard)
+  home() {
+    return 'home';
   }
 }
--- notes/backend/src/auth/guard/auth.guard.ts
+++ notes/backend/src/auth/guard/auth.guard.ts
@@ -1,11 +1,35 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const token = this.getTokenFromHeaders(request);
+    if (!token) {
+      throw new UnauthorizedException('Invalid token: Access denied');
+    }
+    try {
+      const secret = process.env.JWT_SECRET;
+      const payload = await this.jwtService.verifyAsync(token, { secret });
+      request['user'] = payload;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token: Access denied');
+    }
     return true;
+  }
+
+  getTokenFromHeaders(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
